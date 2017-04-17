eef60dbe8b3d0132de72f276715b99999a447d68
manibhushan <manibhushan.cs@gmail.com>
2017-04-16 22:13:54 -0700

    check if valid sudoku - initial commit

--- lc_ValidSudoku/src/lc_ValidSudoku/ValidSudoku.java
+++ lc_ValidSudoku/src/lc_ValidSudoku/ValidSudoku.java
@@ -1,5 +1,11 @@
 package lc_ValidSudoku;
 
+/**
+ * Determine if a Sudoku is valid. The Sudoku board could be partially filled, 
+ * where empty cells are filled with the character '.'.
+ * @author manib
+ *
+ */
 public class ValidSudoku {
 
 	public static void main(String [] args) {
