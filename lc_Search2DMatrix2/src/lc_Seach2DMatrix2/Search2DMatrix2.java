dcc62a797ecd2cca82a74e6408d4954371c85830
manibhushan <manibhushan.cs@gmail.com>
2017-04-16 22:13:08 -0700

    added problem statement comment

--- lc_Search2DMatrix2/src/lc_Seach2DMatrix2/Search2DMatrix2.java
+++ lc_Search2DMatrix2/src/lc_Seach2DMatrix2/Search2DMatrix2.java
@@ -5,6 +5,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+
+For example, consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+ * @author manib
+ *
+ */
 public class Search2DMatrix2 {
 	
 	public int ROWS = 0;
