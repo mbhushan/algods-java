8e44f2c6855a4770e7d38193265935619d86f2e2
manibhushan <manibhushan.cs@gmail.com>
2017-04-27 21:31:27 -0700

    unique paths

--- lc_UniquePaths/src/lc_UniquePaths/UniquePaths.java
+++ lc_UniquePaths/src/lc_UniquePaths/UniquePaths.java
@@ -25,5 +25,29 @@ public class UniquePaths {
 		
 		return dp[row-1][col-1];
 	}
+	
+	public int uniquePathsRec(int m, int n) {
+	    return dfs(0,0,m,n);
+	}
+	 
+	public int dfs(int i, int j, int m, int n){
+	    if(i==m-1 && j==n-1){
+	        return 1;
+	    }
+	 
+	    if(i<m-1 && j<n-1){
+	        return dfs(i+1,j,m,n) + dfs(i,j+1,m,n);
+	    }
+	 
+	    if(i<m-1){
+	        return dfs(i+1,j,m,n);
+	    }
+	 
+	    if(j<n-1){
+	        return dfs(i,j+1,m,n);
+	    }
+	 
+	    return 0;
+	}
 
 }
