b4a3d12f063c2e497749b6e3a8980c414826fdbe
Mani Bhushan <manibhushan.cs@gmail.com>
2018-04-07 20:55:57 -0700

    Test Ground

--- TestGround/src/TestGround.java
+++ TestGround/src/TestGround.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -19,5 +20,47 @@ public class TestGround {
         queue.addAll(listEx);
 
         System.out.println(queue.toString());
+
+        TestGround tg = new TestGround();
+        tg.testListRm();
+    }
+
+    public void testListRm() {
+        List<Node> mylist = new ArrayList<>();
+        mylist.add(new Node("k1", "u1"));
+        mylist.add(new Node("k2", "u2"));
+        mylist.add(new Node("k3", "u3"));
+        mylist.add(new Node("k4", "u4"));
+
+        Node node = new Node("k2", "u2");
+
+//        if (mylist.contains(node)) {
+//            mylist.remove(node);
+//        }
+
+        for (Node n: mylist) {
+           if (n.uuid.equals(node.uuid)) {
+               mylist.remove(n);
+               break;
+           }
+        }
+
+        System.out.println(mylist.toString());
+
+    }
+}
+
+
+class Node {
+    String keyspace;
+    String uuid;
+
+    Node (String keyspace, String uuid) {
+        this.keyspace = keyspace;
+        this.uuid = uuid;
+    }
+
+    public String toString() {
+        return "[" + this.keyspace + ", " + this.uuid + "]";
     }
 }
