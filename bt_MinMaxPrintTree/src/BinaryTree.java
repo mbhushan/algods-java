eab5c7f41d148206fae397296ce1de78a8ed7a1f
Mani Bhushan <manibhushan.cs@gmail.com>
2018-05-13 22:05:47 -0700

    min valur of a binary search tree

---
+++ bt_MinMaxPrintTree/src/BinaryTree.java
@@ -0,0 +1,89 @@
+/**
+ * Created by manib on 5/13/18.
+ */
+public class BinaryTree {
+
+    private Node root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        int [] A = {8, 3, 10, 1, 6, 14, 4, 7, 13 };
+        System.out.println("A.len: " + A.length);
+        bt.buildBST(A);
+        bt.inorder();
+        System.out.println();
+        System.out.println("min value: " + bt.minValue());
+
+    }
+
+    public Integer minValue() {
+        return minValue(this.root);
+    }
+
+    private Integer minValue(Node node) {
+
+        int minValue = Integer.MAX_VALUE;
+        while (node != null) {
+            if (node.data < minValue) {
+                minValue = node.data;
+            }
+            node = node.left;
+        }
+
+        return minValue;
+    }
+
+    public void inorder() {
+        inorder(this.root);
+    }
+
+    private void inorder(Node node) {
+        if (node == null) {
+            return ;
+        }
+        if (node.left != null) {
+            inorder(node.left);
+        }
+        System.out.print(node.data + " ");
+        if (node.right != null) {
+            inorder(node.right);
+        }
+    }
+
+    public void buildBST(int [] A) {
+        if (A == null || A.length < 1) {
+            return;
+        }
+        for (int i=0; i<A.length; i++) {
+            root = insertIntoBST(root, A[i]);
+        }
+    }
+
+    public Node insertIntoBST(Node node, int data) {
+        if (node == null) {
+            return new Node(data);
+        }
+        if (data <= node.data) {
+            node.left = insertIntoBST(node.left, data);
+        } else {
+            node.right = insertIntoBST(node.right, data);
+        }
+        return node;
+    }
+}
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
