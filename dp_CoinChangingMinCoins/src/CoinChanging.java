39d441a8b6329911afeadbe9e443b100e3955694
Mani Bhushan <manibhushan.cs@gmail.com>
2018-06-27 05:26:56 -0700

    coin changing solution

--- dp_CoinChangingMinCoins/src/CoinChanging.java
+++ dp_CoinChangingMinCoins/src/CoinChanging.java
@@ -1,4 +1,84 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ Given a total and coins of certain denomination with infinite supply,
+ what is the minimum number of coins it takes to form this total V.
+
+ */
 
 public class CoinChanging {
+
+    public static void main(String [] args) {
+        CoinChanging cc = new CoinChanging();
+        int [] A = {2, 3, 6, 7};
+        int V = 13;
+
+        cc.changeCoins(A, V);
+    }
+
+    public void changeCoins(int [] A, int total) {
+        if (A == null) {
+            return;
+        }
+        int row = A.length+1;
+        int col = total+1;
+        int [][] T = new int[row][col];
+        for (int c=1; c < col; c++) {
+            T[0][c] = Integer.MAX_VALUE-1;
+        }
+
+        for (int i=1; i<row; i++) {
+            for (int j=1; j<col; j++) {
+                if (A[i-1] <= j) {
+                    T[i][j] = Math.min(T[i-1][j], 1 + T[i][j-A[i-1]]);
+                } else {
+                    T[i][j] = T[i-1][j];
+                }
+            }
+        }
+
+        printDPMatrix(T);
+        System.out.println("min coins required: " + T[row-1][col-1]);
+        printCoins(T, A);
+    }
+
+    public void printCoins(int [][] M, int [] coins) {
+        List<Integer> result = new ArrayList<>();
+        int row = M.length-1;
+        int col = M[0].length-1;
+
+        while (row > 0 && col > 0) {
+            if (M[row][col] == M[row-1][col]) {
+                --row;
+            } else {
+                result.add(row-1);
+                col = col - coins[row-1];
+            }
+        }
+        System.out.println("result indices: " + result);
+        System.out.println("coints are: ");
+        ListIterator<Integer> iter = result.listIterator(result.size());
+        while (iter.hasPrevious()) {
+            int index = iter.previous();
+            System.out.print(coins[index] + " ");
+        }
+        System.out.println();
+    }
+
+    private void printDPMatrix(int [][] M) {
+        int row = M.length;
+        int col = M[0].length;
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (M[i][j] == (Integer.MAX_VALUE-1)) {
+                    System.out.print("X" + " ");
+                } else {
+                    System.out.print(M[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
