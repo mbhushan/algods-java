641d376a8788841baabdb56d367e1af38712a8d9
Mani Bhushan <manibhushan.cs@gmail.com>
2018-06-29 07:57:45 -0700

    min edit distance solution with sample input/output

--- dp_MinEditDistance/src/EditDistance.java
+++ dp_MinEditDistance/src/EditDistance.java
@@ -5,6 +5,24 @@
  Given two strings A & B and operations edit, delete and add, how many minimum
  operations would it take to convert string B to string A.
 
+ ================
+ INPUT / OUTPUT
+ ================
+ DP Matrix:
+ 0 1 2 3 4 5 6
+ 1 0 1 2 3 4 5
+ 2 1 0 1 2 3 4
+ 3 2 1 0 1 2 3
+ 4 3 2 1 1 2 3
+ 5 4 3 2 2 2 3
+ 6 5 4 3 3 3 2
+ 7 6 5 4 4 4 3
+ Min edit required: 3
+ Edits required to convert: COMPUTE to COMMIT
+ del: E
+ update: U -> I
+ update: P -> M
+
  */
 public class EditDistance {
 
@@ -47,6 +65,31 @@ public class EditDistance {
         //print DP matrix
         System.out.println("DP Matrix: ");
         printDPMatrix(T);
+        System.out.println("Min edit required: " + T[s1Len][s2Len]);
+        System.out.println("Edits required to convert: " + S1 + " to " + S2);
+        printEdits(T, S1, S2);
+    }
+
+    public void printEdits(int [][] T, String S1, String S2) {
+        int row = S1.length();
+        int col = S2.length();
+
+        while (row > 0 && col > 0) {
+            if (T[row][col] == T[row-1][col]+1) {
+                System.out.println("del: " + S1.charAt(row-1));
+                --row;
+            } else if (T[row][col] == T[row][col-1]+1) {
+                System.out.println("add: " + S2.charAt(col-1));
+                --col;
+            } else if (T[row][col] == T[row-1][col-1]+1) {
+                System.out.println("update: " + S1.charAt(row-1) + " -> " + S2.charAt(col-1));
+                --row;
+                --col;
+            } else {
+                --row;
+                --col;
+            }
+        }
     }
 
     public void printDPMatrix(int [][] M) {
