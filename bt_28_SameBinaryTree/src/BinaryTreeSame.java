4ff18784e22d8dd8000ed3f2a8635013958d0e1f
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-06 06:57:56 -0700

    Solution: Two binary trees are considered the same if they have identical structure and nodes have the same value.

--- bt_28_SameBinaryTree/src/BinaryTreeSame.java
+++ bt_28_SameBinaryTree/src/BinaryTreeSame.java
@@ -5,16 +5,36 @@ import java.util.Stack;
 
  Two binary trees are considered the same if they have identical structure and nodes have the same value.
 
+ =================
+ INPUT / OUTPUT
+ =================
+ inorder of broken BST:
+ 2 20 6 9 15 5 28 30 34 41
+ swapped nodes are:
+ 20 and 5
+ fixed binary search tree:
+ 2 5 6 9 15 20 28 30 34 41
+
+ inorder of broken BST:
+ 2 6 5 9 15 20 28 30 34 41
+ swapped nodes are:
+ 6 and 5
+ fixed binary search tree:
+ 2 5 6 9 15 20 28 30 34 41
+ is same tree: true
+ is same tree: false
 
  */
 public class BinaryTreeSame {
 
     private Node root;
     private Node root2;
+    private Node root3;
 
     BinaryTreeSame() {
         this.root = null;
         this.root2 = null;
+        this.root3 = null;
     }
 
     public static void main(String [] args) {
@@ -29,6 +49,38 @@ public class BinaryTreeSame {
         System.out.println();
         System.out.println("inorder of broken BST: ");
         bt.inorder2();
+
+        System.out.println();
+        bt.isSameTree1();
+
+        bt.buildBT3();
+        bt.isSameTree2();
+    }
+
+    public void isSameTree1() {
+        boolean ans = isSameTree(this.root, this.root2);
+        System.out.println("is same tree: " + ans);
+    }
+
+    public void isSameTree2() {
+        boolean ans = isSameTree(this.root2, this.root3);
+        System.out.println("is same tree: " + ans);
+    }
+
+    private boolean isSameTree(Node node1, Node node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+
+        if (node1.data != node2.data) {
+            return false;
+        }
+
+        return isSameTree(node1.left, node2.left) && isSameTree(node1.right, node2.right);
     }
 
     public void inorder1() {
@@ -129,6 +181,23 @@ public class BinaryTreeSame {
         this.root2.right.right = new Node(34);
 
         this.root2.left.left.left = new Node(2);
+        this.root2.left.left.right = new Node(5);
+
+        this.root2.right.right.left = new Node(30);
+        this.root2.right.right.right = new Node(41);
+
+    }
+
+    public void buildBT3() {
+        this.root2 = new Node(15);
+        this.root2.left = new Node(9);
+        this.root2.right = new Node(28);
+
+        this.root2.left.left = new Node(6);
+        this.root2.right.left = new Node(20);
+        this.root2.right.right = new Node(34);
+
+        this.root2.left.left.left = new Node(3);
         this.root2.left.left.right = new Node(5);
 
         this.root2.right.right.left = new Node(30);
