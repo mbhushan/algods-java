7716237ac80b3ec6c28126e294465efb8fead59b
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-05 18:49:43 -0700

    Setup: Given a binary tree, find its maximum and minimum depth

--- bt_26_LongestShortestPath/src/BinaryTreePathLengths.java
+++ bt_26_LongestShortestPath/src/BinaryTreePathLengths.java
@@ -1,4 +1,97 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
-public class BinaryTreePathLenghts {
+/**
+ LeetCode – Maximum Depth of Binary Tree (Java)
+
+ Given a binary tree, find its maximum depth.
+
+ The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ Example binary tree
+
+              8
+          /       \
+         3        10
+       /  \       \
+      1    6      14
+         /  \   /
+        4   7  13
+
+ =====================
+ INPUT / OUTPUT
+ ====================
+
+ */
+public class BinaryTreePathLengths {
+
+    private Node root;
+
+    BinaryTreePathLengths() {
+        this.root = null;
+    }
+
+    public static void main(String[] args) {
+        BinaryTreePathLengths bt = new BinaryTreePathLengths();
+        // https://en.wikipedia.org/wiki/Binary_search_tree#/media/File:Binary_search_tree.svg
+        int [] A = {8, 3, 10, 1, 6, 14, 4, 7, 13};
+
+        bt.buildBST(A);
+        bt.inorder();
+        System.out.println();
+    }
+
+    public void inorder() {
+        System.out.println("inorder traversal itertive: ");
+        inorderIterative(this.root);
+    }
+
+    private void inorderIterative(Node node) {
+        Stack<Node> stack = new Stack<>();
+
+        while (node != null || !stack.isEmpty()) {
+            if (node == null) {
+                node = stack.pop();
+                System.out.print(node.data + " ");
+                node = node.right;
+            }
+            if (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+    }
+
+    public void buildBST(int [] A) {
+        if (A == null) {
+            return;
+        }
+
+        for (int i=0; i<A.length; i++) {
+            this.root = insertIntoBST(this.root, A[i]);
+        }
+    }
+
+    public Node insertIntoBST(Node node, int data) {
+        if (node == null) {
+            return new Node(data);
+        }
+        if (data <= node.data) {
+            node.left = insertIntoBST(node.left, data);
+        } else {
+            node.right = insertIntoBST(node.right, data);
+        }
+        return node;
+    }
+}
+
+class Node {
+    Integer data;
+    Node left;
+    Node right;
+
+    Node(Integer data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
 }
