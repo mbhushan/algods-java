25f3bfa6be456ced6ee4863ba951114c54b72540
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-07 12:29:02 -0700

    Setup:  Given a binary tree, find the largest subtree which is a Binary Search Tree

--- bt_32_LargestBST/src/BSTLargest.java
+++ bt_32_LargestBST/src/BSTLargest.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+/**
+ LeetCode – Largest BST Subtree (Java)
+
+ Given a binary tree, find the largest subtree which is a Binary Search Tree (BST),
+ where largest means subtree with largest number of nodes in it.
+ */
 
 public class BSTLargest {
+
+    public static void main(String[] args) {
+
+    }
+
 }
