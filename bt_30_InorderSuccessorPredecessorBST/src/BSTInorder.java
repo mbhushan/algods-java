ef7497e5e7cbf32d2304504ef0517d8a661cb106
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-06 22:38:58 -0700

    setup: Inorder Successor in BST

--- bt_30_InorderSuccessorPredecessorBST/src/BSTInorder.java
+++ bt_30_InorderSuccessorPredecessorBST/src/BSTInorder.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+/**
+ LeetCode – Inorder Successor in BST (Java)
 
+ Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+ */
 public class BSTInorder {
+
+    public static void main(String[] args) {
+
+    }
 }
