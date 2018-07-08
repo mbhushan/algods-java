305090e3feff631b4851a414e9e9e4e16f2cef56
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-08 08:45:04 -0700

    Given a m * n matrix, if an element is 0, set its entire row and column to 0.

---
+++ matrix_01_SetMatrixZero/src/MatrixSetZero.java
@@ -0,0 +1,15 @@
+/**
+ LeetCode – Set Matrix Zeroes (Java)
+
+ Given a m * n matrix, if an element is 0, set its entire row and column to 0.
+ Do it in place.
+
+
+ */
+
+public class MatrixSetZero {
+
+    public static void main(String[] args) {
+
+    }
+}
