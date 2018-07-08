b99b7d24d1f577f8b6f41c7f4f2f4d33719cde38
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-08 10:51:17 -0700

    Setup: Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order

--- matrix_02_MatrixSpiral2/src/MatrixSpiral2.java
+++ matrix_02_MatrixSpiral2/src/MatrixSpiral2.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+/**
+ LeetCode – Spiral Matrix II (Java)
+
+ Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order. For example, given n = 4,
+
+ [
+ [1,   2,  3, 4],
+ [12, 13, 14, 5],
+ [11, 16, 15, 6],
+ [10,  9,  8, 7]
+ ]
+
+ ====================
+ INPUT / OUTPUT
+ ===================
+
+ */
 
 public class MatrixSpiral2 {
+
+    public static void main(String[] args) {
+
+    }
 }
