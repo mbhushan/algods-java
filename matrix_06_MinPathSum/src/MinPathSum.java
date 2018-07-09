b990f6d9f6b6ef95f638fc64d5557ef5e0ad98b2
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-09 13:22:20 -0700

    Setup: Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.

--- matrix_06_MinPathSum/src/MinPathSum.java
+++ matrix_06_MinPathSum/src/MinPathSum.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+/**
+ LeetCode – Minimum Path Sum (Java)
+
+ Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right
+ which minimizes the sum of all numbers along its path.
+ */
 
 public class MinPathSum {
+
+    public static void main(String [] args) {
+        MinPathSum mps = new MinPathSum();
+
+        int M[][] = {
+                {1, 2, 3},
+                {4, 8, 2},
+                {1, 5, 3}
+        };
+
+
+
+    }
+
+    public void 
 }
