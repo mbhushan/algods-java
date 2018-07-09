197da50b146bb345ab469d03c6cbcd2fa6007dd4
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-09 07:24:17 -0700

    Setup: Determine if a Sudoku is valid.

--- matrix_05_SudokuValidity/src/SudokuCheck.java
+++ matrix_05_SudokuValidity/src/SudokuCheck.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+/**
+ LeetCode – Valid Sudoku (Java)
+
+ Determine if a Sudoku is valid. The Sudoku board could be partially filled,
+ where empty cells are filled with the character '.'.
+
+ ===============
+ INPUT / OUTPUT
+ ===============
+
+ */
+
 
 public class SudokuCheck {
+
+    public static void main(String [] args) {
+
+    }
 }
