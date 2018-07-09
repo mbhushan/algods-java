5ecdc6c6bcda867115f08c9c9e80d0bd56a39345
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-09 16:09:58 -0700

    Solution: move down/right - min cost path

--- matrix_06_MinPathSum/src/MinPathSum.java
+++ matrix_06_MinPathSum/src/MinPathSum.java
@@ -16,9 +16,57 @@ public class MinPathSum {
                 {1, 5, 3}
         };
 
+        System.out.println("input matrix: ");
 
-
+        mps.printMatrix(M);
+        System.out.println();
+        mps.minPathSum2Directions(M);
     }
 
-    public void 
+    public void minPathSum2Directions(int [][] M) {
+        int row = M.length;
+        int col = M[0].length;
+        int r = 0;
+        int c = 0;
+        int pathCost = minPathSum2Directions(M, r, c, row-1, col-1);
+        System.out.println("min path cost (movement: right and down): " + pathCost);
+    }
+
+    private int minPathSum2Directions(int [][] M, int r, int c, int row, int col) {
+        if (r == row && c == col) {
+            return M[row][col];
+        }
+
+        if (r < row && c < col) {
+            int cost1 = M[r][c] + minPathSum2Directions(M, r+1, c, row, col);
+            int cost2 = M[r][c] + minPathSum2Directions(M, r, c+1, row, col);
+            return Math.min(cost1, cost2);
+        }
+
+        if (r < row) {
+            return M[r][c] + minPathSum2Directions(M, r+1, c, row, col);
+        }
+
+        if (c < col) {
+            return M[r][c] + minPathSum2Directions(M, r, c+1, row, col);
+        }
+
+        return 0;
+    }
+
+    public void printMatrix(int [][] M) {
+        if (M == null) {
+            return;
+        }
+        int row = M.length;
+        int col = M[0].length;
+
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                System.out.print(M[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
