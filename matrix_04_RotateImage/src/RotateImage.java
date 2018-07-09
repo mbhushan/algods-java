4c0fb58eb587860510ea517671e3d6d1e36351ee
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-09 07:12:20 -0700

    Setup: Rotate the image by 90 degrees (clockwise).

--- matrix_04_RotateImage/src/RotateImage.java
+++ matrix_04_RotateImage/src/RotateImage.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+/**
+ LeetCode – Rotate Image (Java)
+
+ You are given an n x n 2D matrix representing an image.
+
+ Rotate the image by 90 degrees (clockwise).
+
+ */
 
 public class RotateImage {
+
+    public static void main(String [] args) {
+        int [][] M = {
+                {1,   4,  7, 11, 15},
+                {2,   5,  8, 12, 19},
+                {3,   6,  9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30},
+        };
+
+        RotateImage ri = new RotateImage();
+    }
+
+
 }
