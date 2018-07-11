3d711892eec3658f9113a273640ebe146ae7ac88
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-11 07:44:08 -0700

    Solution: unique paths for robots in matrix with obstacles - can travel in right/bottom directions only

--- matrix_07_UniquePaths2/src/UniquePaths2.java
+++ matrix_07_UniquePaths2/src/UniquePaths2.java
@@ -15,11 +15,86 @@
  [0,0,0]
  ]
  the total number of unique paths is 2.
+
+ ===============
+ INPUT / OUTPUT
+ ==============
+ printing DP matrix:
+ 1 1 1
+ 1 0 1
+ 1 1 2
+
+ find unique paths: 2
  */
 
 public class UniquePaths2 {
 
     public static void main(String[] args) {
 
+        UniquePaths2 up = new UniquePaths2();
+
+        int [][] paths = {
+                   {0,0,0},
+                    {0,1,0},
+                    {0,0,0}
+                };
+
+        int result = up.findUniquePathsDP(paths);
+
+        System.out.println("find unique paths: " + result);
+
+
+
+    }
+
+    public int findUniquePathsDP(int [][] M) {
+
+        int row = M.length;
+        int col = M[0].length;
+
+        int [][] T = new int[row][col];
+
+        for (int i=0; i<row; i++) {
+            if (M[i][0] != 1) {
+                T[i][0] = 1;
+            }
+        }
+
+        for (int i=0; i<col; i++) {
+            if (M[0][i] != 1) {
+                T[0][i] = 1;
+            }
+        }
+
+        for (int i=1; i<row; i++) {
+            for (int j=1; j<col; j++) {
+                if (M[i][j] == 1) {
+                    T[i][j] = 0;
+                } else {
+                    T[i][j] = T[i-1][j] + T[i][j-1];
+                }
+            }
+        }
+
+        System.out.println("printing DP matrix: ");
+        printMatrix(T);
+        System.out.println();
+
+        return T[row-1][col-1];
+    }
+
+    public void printMatrix(int [][] M) {
+        if (M == null) {
+            return;
+        }
+        int row = M.length;
+        int col = M[0].length;
+
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                System.out.print(M[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
