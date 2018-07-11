6e64744a25f92f2c0b8a9e6938097444a1895638
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-10 22:14:03 -0700

    DP Solution: unique paths in a mtrix for robot travelling right / bottom

--- matrix_07_UniquePaths/src/UniquePaths.java
+++ matrix_07_UniquePaths/src/UniquePaths.java
@@ -18,10 +18,32 @@ public class UniquePaths {
 
         System.out.println("unique paths for (" + row + ", " + col + ")");
         System.out.println(up.uniquePathsDFS(row, col));
+
+        System.out.println();
+
+        System.out.println("DP approach for finding unique paths: " + up.uniquePathsDP(row, col));
     }
 
     public int uniquePathsDP(int m ,int n) {
-        return 0;
+
+        int [][] T = new int[m][n];
+        T[0][0] = 1;
+
+        for (int i=1; i<m; i++) {
+            T[0][i] = 1;
+        }
+
+        for (int i=1; i<n; i++) {
+            T[i][0] = 1;
+        }
+
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                T[i][j] = T[i-1][j] + T[i][j-1];
+            }
+        }
+
+        return T[m-1][n-1];
     }
 
     public int uniquePathsDFS(int m, int n) {
