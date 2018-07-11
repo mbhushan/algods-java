ed200d63d8f3e12d1a2aaf2200b28f991354c0ae
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-10 22:18:22 -0700

    Setup: unique paths for robots in matrix with obstacles - can travel in right/bottom directions only

--- matrix_07_UniquePaths2/src/UniquePaths2.java
+++ matrix_07_UniquePaths2/src/UniquePaths2.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+/**
+ LeetCode – Unique Paths II (Java)
+
+ Follow up for "Unique Paths":
+
+ Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+
+ An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+ For example, there is one obstacle in the middle of a 3x3 grid as illustrated below,
+
+ [
+ [0,0,0],
+ [0,1,0],
+ [0,0,0]
+ ]
+ the total number of unique paths is 2.
+ */
 
 public class UniquePaths2 {
+
+    public static void main(String[] args) {
+
+    }
 }
