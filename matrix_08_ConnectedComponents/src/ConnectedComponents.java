b873ed197e3981509f391b399334ccb37efbbf9f
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-11 22:47:58 -0700

    Setup: Connected components

---
+++ matrix_08_ConnectedComponents/src/ConnectedComponents.java
@@ -0,0 +1,20 @@
+/**
+ LeetCode – Number of Connected Components in an Undirected Graph (Java)
+
+ Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes),
+ write a function to find the number of connected components in an undirected graph.
+
+ Example 1:
+ 0 3
+ | |
+ 1 --- 2 4
+ Given n = 5 and edges = [[0, 1], [1, 2], [3, 4]], return 2.
+ */
+
+public class ConnectedComponents {
+    public static void main(String[] args) {
+
+    }
+
+
+}
