bdcc0fc7b4438a382ee82662cf2dbbe37ad934d5
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-13 22:00:50 -0700

    Solution: Number of Connected Components in an Undirected Graph

--- matrix_08_ConnectedComponents/src/ConnectedComponents.java
+++ matrix_08_ConnectedComponents/src/ConnectedComponents.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  LeetCode – Number of Connected Components in an Undirected Graph (Java)
 
@@ -9,11 +11,61 @@
  | |
  1 --- 2 4
  Given n = 5 and edges = [[0, 1], [1, 2], [3, 4]], return 2.
+
+ ================
+ INPUT / OUTPUT
+ ================
+
+ total connected components: 2
+
  */
 
 public class ConnectedComponents {
+
     public static void main(String[] args) {
 
+        ConnectedComponents cc = new ConnectedComponents();
+
+        int n  = 5;
+
+        int [][] edges = {
+                {0, 1},
+                {1, 2},
+                {3, 4},
+                //{4, 2}
+        };
+
+        cc.getConnectedComponents(edges, n);
+    }
+
+    public void getConnectedComponents(int [][] edges, int n) {
+
+        int [] parents = new int[n];
+        Arrays.fill(parents, -1);
+        int count = 5;
+
+        for (int i=0; i<edges.length; i++) {
+            int [] col = edges[i];
+
+            int p = col[0];
+            int q = col[1];
+            //++count;
+
+            if ((parents[p] == -1 && parents[q] == -1) || (getParent(parents, p) != getParent(parents, q))) {
+                parents[q] = p;
+                --count;
+            }
+        }
+
+        System.out.println("total connected components: " + count);
+    }
+
+    private int getParent(int [] parents, int x) {
+        while (parents[x] != -1) {
+            x = parents[x];
+        }
+
+        return x;
     }
 
 
