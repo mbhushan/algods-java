b35d457e294dac6ef76071e5b8ac8766cd9e3043
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-13 22:53:48 -0700

    Setup: Maximum size rectangle binary sub-matrix with all 1s

--- matrix_10_MaxRectangleOf1s/src/MaxRectangle.java
+++ matrix_10_MaxRectangleOf1s/src/MaxRectangle.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+/**
+ Maximum size rectangle binary sub-matrix with all 1s
+ Given a binary matrix, find the maximum size rectangle binary-sub-matrix with all 1’s.
+
+ Input :
+ 0 1 1 0
+ 1 1 1 1
+ 1 1 1 1
+ 1 1 0 0
+
+ Output :
+ 1 1 1 1
+ 1 1 1 1
+ ===================
+ INPUT / OUTPUT
+ ===================
+ */
 
 public class MaxRectangle {
+
+    public static void main(String [] args) {
+        MaxRectangle mr = new MaxRectangle();
+
+        int [][] M = {
+                {0, 1, 1, 0},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1},
+                {1, 1, 0, 0},
+        };
+
+    }
 }
