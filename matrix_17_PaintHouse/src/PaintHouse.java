7805995f82a9a940bbf6b9bfcfde1edd1627b922
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-20 22:52:05 -0700

    Setup: min cost to paint house: There are a row of n houses, each house can be painted with one of the three colors: red, blue or green.

--- matrix_17_PaintHouse/src/PaintHouse.java
+++ matrix_17_PaintHouse/src/PaintHouse.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+/**
+ LeetCode – Paint House (Java)
+
+ There are a row of n houses, each house can be painted with one of the three colors: red, blue or green.
+ The cost of painting each house with a certain color is different. You have to paint all the houses such
+ that no two adjacent houses have the same color.
+
+ The cost of painting each house with a certain color is represented by a n x 3 cost matrix.
+ For example, costs[0][0] is the cost of painting house 0 with color red; costs[1][2] is the cost
+ of painting house 1 with color green, and so on... Find the minimum cost to paint all houses.
+ */
 
 public class PaintHouse {
+
+    public static void main(String[] args) {
+        PaintHouse ph = new PaintHouse();
+
+        int [][] M = {
+                {7, 5, 10},
+                {3, 6, 1},
+                {8, 7, 4},
+                {6, 2, 9},
+                {1, 4, 7},
+                {2, 3, 6},
+        };
+
+    }
+
+    public void paintHouse(int [][] M) {
+
+    }
 }
