645ea8b90bf269212bb0f50168a4cc86e7f96c89
Mani Bhushan <manibhushan.cs@gmail.com>
2018-07-26 22:13:54 -0700

    Solution:  Check validity of tic tac toe game

--- matrix_20_TicTacToe/src/TicTacToe.java
+++ matrix_20_TicTacToe/src/TicTacToe.java
@@ -1,7 +1,9 @@
 /**
- A Tic-Tac-Toe board is given as a string array board. Return True if and only if it is possible to reach this board position during the course of a valid tic-tac-toe game.
+ A Tic-Tac-Toe board is given as a string array board. Return True if and only if it is possible
+ to reach this board position during the course of a valid tic-tac-toe game.
 
- The board is a 3 x 3 array, and consists of characters " ", "X", and "O".  The " " character represents an empty square.
+ The board is a 3 x 3 array, and consists of characters " ", "X", and "O".
+ The " " character represents an empty square.
 
  Here are the rules of Tic-Tac-Toe:
 
@@ -11,6 +13,7 @@
  The game ends when there are 3 of the same (non-empty) character filling any row, column, or diagonal.
  The game also ends if all squares are non-empty.
  No more moves can be played if the game is over.
+
  Example 1:
  Input: board = ["O  ", "   ", "   "]
  Output: false
@@ -40,6 +43,121 @@ public class TicTacToe {
     public static void main(String[] args) {
         TicTacToe tt = new TicTacToe();
 
+        char [][] board = {
+                {'X', 'X', 'O'},
+                {'O', 'O', 'X'},
+                {'X', 'O', 'X'}
+        };
 
+        System.out.println("input board configuration: ");
+        tt.printMatrix(board);
+        System.out.println();
+
+
+    }
+
+    public boolean checkValidity(char [][] M) {
+        if (M == null) {
+            return false;
+        }
+
+        int xCount = 0;
+        int oCount = 0;
+
+        for (int i=0; i<M.length; i++) {
+            for (int j=0; j<M.length; j++) {
+                if (M[i][j] == 'X') {
+                    ++xCount;
+                } else if (M[i][j] == 'O') {
+                    ++oCount;
+                }
+            }
+        }
+
+        //check validity conditions
+
+        //1. if xCount == oCount or xCount = oCount+1
+        if (xCount == oCount || xCount == oCount+1) {
+            
+        }
+
+
+
+        return true;
+    }
+
+    public boolean checkWin(char [][] M, char player, int count) {
+        if (count < 3) {
+            return false;
+        }
+
+        //check all rows and cols
+        int r = 0;
+
+        while (r++ < M.length) {
+            count = 0;
+            for (int i=0; i<M.length; i++) {
+                if (M[r][i] == player) {
+                    ++count;
+                }
+            }
+            if (count == 3) {
+                return true;
+            }
+            count = 0;
+            for (int i=0; i<M.length; i++) {
+                if (M[i][r] == player) {
+                    ++count;
+                }
+            }
+            if (count == 3) {
+                return true;
+            }
+        }
+
+        //check back diagonal
+        for (int i=0; i<M.length; i++) {
+            count = 0;
+            for (int j=0; j<M.length; j++) {
+                if (i == j && M[i][j] == player) {
+                    ++count;
+                }
+            }
+            if (count == 3) {
+                return true;
+            }
+        }
+
+        //check front diagonal
+        for (int i=0; i<M.length; i++) {
+            count = 0;
+            for (int j=0; j<M.length; j++) {
+                if ((i+j == M.length-1) && M[i][j] == player) {
+                    ++count;
+                }
+            }
+            if (count == 3) {
+                return true;
+            }
+        }
+
+
+        return false;
+
+    }
+
+    public void printMatrix(char [][] M) {
+        if (M == null) {
+            return;
+        }
+        int row = M.length;
+        int col = M[0].length;
+
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                System.out.print(M[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
