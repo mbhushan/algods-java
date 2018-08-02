6bafeea7971db1a0b5558a18fe1c99f9f0978dae
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-01 22:18:32 -0700

    Setup: Generate all combination which prints star in place of absent characters.

--- rec_05_StarCombinations/src/StarCombinations.java
+++ rec_05_StarCombinations/src/StarCombinations.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.util.List;
 
+/**
+ Generate all combination which prints star in place of absent characters.
+ * e.g a, b c
+ * * * *
+ * a * *
+ * a b *
+ * a b c
+ * * b *
+ * * b c
+ * * * c
+ *
+ * Idea is to store the index of values in used[] array. So just
+ * like regular combination if used is set print it else print *
+
+ */
 public class StarCombinations {
+
+    public static void main(String [] args) {
+        StarCombinations sc = new StarCombinations();
+
+        int [] A = {1, 2, 3};
+
+        sc.genCombinations(A);
+    }
+
+    public void genCombinations(int [] A) {
+
+    }
+
+    private void genCombinations(int [] A, int r, List<Integer> buff) {
+
+    }
 }
