0b699ac453c949a897d33498d4ef4b50d455f676
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-02 19:49:16 -0700

    Setup: Print all possible words from phone digits

--- rec_08_KeyPadPermutations/src/KeyPadPermutations.java
+++ rec_08_KeyPadPermutations/src/KeyPadPermutations.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+/**
+ Print all possible words from phone digits
+ Before advent of QWERTY keyboards, texts and numbers were placed on the same key.
+ For example 2 has “ABC” if we wanted to write anything starting with ‘A’ we need to type key 2 once.
+ If we wanted to type ‘B’, press key 2 twice and thrice for typing ‘C’. below is picture of such keypad.
+
+ Mobile-keypad
+
+ Given a keypad as shown in diagram, and a n digit number, list all words which are possible by pressing these numbers.
+ For example if input number is 234, possible words which can be formed are (Alphabetical order):
+
+ adg adh adi aeg aeh aei afg afh afi bdg bdh bdi beg beh bei bfg bfh bfi cdg cdh cdi ceg ceh cei cfg cfh cfi
+
+ */
 
 public class KeyPadPermutations {
+
+    public static void main(String[] args) {
+        KeyPadPermutations kp = new KeyPadPermutations();
+
+    }
 }
