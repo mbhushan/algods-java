83c92052178223af575c71d50f726ea114f69e9f
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-03 13:44:28 -0700

    Setup: Imagine x is an operand and * is a binary operator.  We say a string of x and * follows Reverse Polish notation if it is a postfix notation.

--- rec_09_MinEditsRPN/src/MinEditRPN.java
+++ rec_09_MinEditsRPN/src/MinEditRPN.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+/**
+ Imagine x is an operand and * is a binary operator.
+ We say a string of x and * follows Reverse Polish notation if it is a postfix notation.
+
+ For example strings xx*, x, and xx*xx** follow Reverse Polish notation.
+
+ Given a string of x and *, how many insert, delete, and replace
+ operations are needed to make the string follow the RPN.
+
+ For example, xx* need 0 operation to follow RPN since it already follows RPN.
+ x*x needs two operations to become xx* which follows RPN.
+ *xx* needs one operation to become xx* which follows RPN.
+
+ Your algorithm should work for a string of size up to 100
+
+ */
 
 public class MinEditRPN {
+
+    public static void main(String[] args) {
+        MinEditRPN me = new MinEditRPN();
+
+        String S = "*x*x";
+
+    }
+
+    public void minEdit(char [] S) {
+
+    }
+
+
 }
