87a82a996beb809bf638239b13c695b0895369ec
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-07 22:19:14 -0700

    Solution: N Queen problem

--- rec_10_NQueenProblem/src/NQueen.java
+++ rec_10_NQueenProblem/src/NQueen.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  The N Queen is the problem of placing N chess queens on an N×N chessboard so that no two queens
  attack each other. For example, following is a solution for 4 Queen problem.
@@ -19,5 +22,60 @@ public class NQueen {
     public static void main(String[] args) {
         NQueen nq = new NQueen();
 
+        nq.solveNQueen(4);
+
+    }
+
+    public void solveNQueen(int n) {
+        List<Cell> result = new ArrayList<>();
+        solveNQueen(n, 0, result);
+    }
+
+    private boolean solveNQueen(int N, int row, List<Cell> result) {
+
+        if (row >= N) {
+            System.out.println(result);
+            return true;
+        }
+
+        for (int col=0; col<N; col++) {
+            boolean foundSafe = true;
+            //check if current cell conflicts with prev queen placements.
+            for (Cell cell: result) {
+                if (cell.row == row || cell.col == col || ((cell.row + cell.col) == (row+col))
+                        || ((cell.col - cell.row) == (col-row))) {
+                   foundSafe = false;
+                   break;
+                }
+            }
+            if (foundSafe) {
+                Cell cell = new Cell(row, col);
+                result.add(cell);
+                boolean flag = solveNQueen(N, row + 1, result);
+                if (flag) {
+                    return true;
+                }
+                result.remove(result.size()-1);
+            }
+
+        }
+        return false;
+    }
+
+
+}
+
+class Cell {
+    int row;
+    int col;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.row + ", " + this.col + "]";
     }
 }
