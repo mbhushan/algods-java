f1082e0a58e1c57eeba32429ceb4dcee90523809
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-07 22:43:45 -0700

    N Queen All possible placements

--- rec_10_NQueenProblem/src/NQueen.java
+++ rec_10_NQueenProblem/src/NQueen.java
@@ -15,6 +15,16 @@ import java.util.List;
  { 1,  0,  0,  0}
  { 0,  0,  1,  0}
 
+ ==================
+ INPUT / OUTPUT
+ ==================
+ solving n-queen for N: 4
+ Queen placements: [[0, 1], [1, 3], [2, 0], [3, 2]]
+
+ All Possible Queen placements:
+ [[0, 1], [1, 3], [2, 0], [3, 2]]
+ [[0, 2], [1, 0], [2, 3], [3, 1]]
+
  */
 
 public class NQueen {
@@ -31,7 +41,39 @@ public class NQueen {
         System.out.println("solving n-queen for N: " + n);
         System.out.print("Queen placements: ");
         solveNQueen(n, 0, result);
+        System.out.println();
 
+        result.clear();
+        System.out.println("All Possible Queen placements: ");
+        solveNQueenAllSolution(n, 0, result);
+
+    }
+
+    private void solveNQueenAllSolution(int N, int row, List<Cell> result) {
+
+        if (row >= N) {
+            System.out.println(result);
+            return;
+        }
+
+        for (int col = 0; col < N; col++) {
+            //check if current cell conflicts with prev queen placements.
+            boolean flag = true;
+            for (Cell cell : result) {
+                if (cell.row == row || cell.col == col || ((cell.row + cell.col) == (row + col))
+                        || ((cell.col - cell.row) == (col - row))) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (!flag) {
+                continue;
+            }
+            Cell cell = new Cell(row, col);
+            result.add(cell);
+            solveNQueenAllSolution(N, row + 1, result);
+            result.remove(result.size() - 1);
+        }
     }
 
     private boolean solveNQueen(int N, int row, List<Cell> result) {
