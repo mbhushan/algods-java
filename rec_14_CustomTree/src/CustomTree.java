f13af07e01e4ff8ea09e7ebe30e1b97ae1532c22
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-09 15:41:08 -0700

    Setup:  Print the tree that would form when each pair of these links that has the same

--- rec_14_CustomTree/src/CustomTree.java
+++ rec_14_CustomTree/src/CustomTree.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
+/**
+ Custom Tree Problem
+ You are given a set of links, e.g.
+
+ a ---> b
+ b ---> c
+ b ---> d
+ a ---> e
+
+ Print the tree that would form when each pair of these links that has the same
+ character as start and end point is joined together. You have to maintain fidelity w.r.t.
+ the height of nodes, i.e. nodes at height n from root should be printed at same row or column.
+ For set of links given above, tree printed should be –
+
+ -->a
+ |-->b
+ |   |-->c
+ |   |-->d
+ |-->e
+
+ Note that these links need not form a single tree; they could form, ahem, a forest. Consider the following links
+
+ a ---> b
+ a ---> g
+ b ---> c
+ c ---> d
+ d ---> e
+ c ---> f
+ z ---> y
+ y ---> x
+ x ---> w
+ The output would be following forest.
+
+ -->a
+ |-->b
+ |   |-->c
+ |   |   |-->d
+ |   |   |   |-->e
+ |   |   |-->f
+ |-->g
+
+ -->z
+ |-->y
+ |   |-->x
+ |   |   |-->w
+ You can assume that given links can form a tree or forest of trees only, and there are no duplicates among links.
+ */
 
 public class CustomTree {
+
+    public static void main(String[] args) {
+        CustomTree ct = new CustomTree();
+
+        String [] links1 = {"a b", "b c", "b d", "a e"};
+
+        String [] links2 = {"a b", "a g", "b c", "c d", "d e", "c f",
+                "z y", "y x", "x w"};
+    }
 }
