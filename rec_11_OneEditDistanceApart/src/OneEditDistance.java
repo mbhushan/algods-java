17ddbf1a7cfe10d2bd7911b3bb4e4adf68befad7
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-08 22:51:39 -0700

    Setup: Check if edit distance between two strings is one

--- rec_11_OneEditDistanceApart/src/OneEditDistance.java
+++ rec_11_OneEditDistanceApart/src/OneEditDistance.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+/**
+ Check if edit distance between two strings is one
+ An edit between two strings is one of the following changes.
+
+ Add a character
+ Delete a character
+ Change a character
+ Given two string s1 and s2, find if s1 can be converted to s2 with exactly one edit.
+ Expected time complexity is O(m+n) where m and n are lengths of two strings.
+
+ Examples:
+
+ Input:  s1 = "geeks", s2 = "geek"
+ Output: yes
+ Number of edits is 1
+
+ Input:  s1 = "geeks", s2 = "geeks"
+ Output: no
+ Number of edits is 0
+
+ Input:  s1 = "geaks", s2 = "geeks"
+ Output: yes
+ Number of edits is 1
+
+ Input:  s1 = "peaks", s2 = "geeks"
+ Output: no
+ Number of edits is 2
+
+ */
 
 public class OneEditDistance {
+
+    public static void main(String[] args) {
+        
+    }
 }
