0f6321952f335b59592d174fccb765e96bd6ef0d
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-13 22:57:10 -0700

    setup: Print all distinct permutations of a given string with duplicates

--- rec_15_DistinctPermutations/src/DistinctPermutations.java
+++ rec_15_DistinctPermutations/src/DistinctPermutations.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+/**
+ Print all distinct permutations of a given string with duplicates
+ Given a string that may contain duplicates, write a function to print all permutations
+ of given string such that no permutation is repeated in output.
+
+ Examples:
+
+ Input:  str[] = "AB"
+ Output: AB BA
+
+ Input:  str[] = "AA"
+ Output: AA
+
+ Input:  str[] = "ABC"
+ Output: ABC ACB BAC BCA CBA CAB
+
+ Input:  str[] = "ABA"
+ Output: ABA AAB BAA
+
+ Input:  str[] = "ABCA"
+ Output: AABC AACB ABAC ABCA ACBA ACAB BAAC BACA
+ BCAA CABA CAAB CBAA
+ */
 
 public class DistinctPermutations {
+
+    public static void main(String[] args) {
+        DistinctPermutations dp = new DistinctPermutations();
+
+        String [] inputs = {
+                "AB", "AA", "ABC", "ABA", "ABCA"
+        };
+    }
+
 }
