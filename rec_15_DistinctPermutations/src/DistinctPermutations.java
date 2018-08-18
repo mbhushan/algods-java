c58468f1cb386c6025a1e7e18d6c53978281b44d
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-17 22:30:35 -0700

    Solution: Print all distinct permutations of a given string with duplicates

--- rec_15_DistinctPermutations/src/DistinctPermutations.java
+++ rec_15_DistinctPermutations/src/DistinctPermutations.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  Print all distinct permutations of a given string with duplicates
@@ -20,8 +23,25 @@ import java.util.List;
  Output: ABA AAB BAA
 
  Input:  str[] = "ABCA"
- Output: AABC AACB ABAC ABCA ACBA ACAB BAAC BACA
- BCAA CABA CAAB CBAA
+ Output: AABC AACB ABAC ABCA ACBA ACAB BAAC BACA BCAA CABA CAAB CBAA
+
+ =========================
+ INPUT / OUTPUT
+ ========================
+ input: AB
+ output: AB BA
+
+ input: AA
+ output: AA
+
+ input: ABC
+ output: ABC ACB BAC BCA CBA CAB
+
+ input: ABA
+ output: AAB ABA BAA
+
+ input: ABCA
+ output: AABC AACB ABAC ABCA ACBA ACAB BAAC BACA BCAA CABA CAAB CBAA
  */
 
 public class DistinctPermutations {
@@ -34,16 +54,47 @@ public class DistinctPermutations {
         };
 
         for (String A: inputs) {
+            System.out.println("input: " + A);
+            System.out.print("output: ");
             dp.permute(A.toCharArray());
+            System.out.println();
         }
+
+        //dp.permute("ABCA".toCharArray());
     }
 
     public void permute(char [] A) {
-
+        Arrays.sort(A);
+        permute(A, 0, new ArrayList<>());
+        System.out.println();
     }
 
-    private void permute(char [] A, List<Character> result) {
+    private void permute(char [] A, int index, List<Character> result) {
+        if (result.size() == A.length) {
+            System.out.print(result.stream().map(String::valueOf).collect(Collectors.joining()) + " ");
+        }
 
+        if (index >= A.length) {
+            return;
+        }
+
+        for (int i=index; i<A.length; i++) {
+            if (i != index && A[i] == A[index]) {
+                continue;
+            }
+            result.add(A[i]);
+
+            char tmp = A[i];
+            A[i] = A[index];
+            A[index] = tmp;
+            permute(A, index+1, result);
+
+            tmp = A[i];
+            A[i] = A[index];
+            A[index] = tmp;
+
+            result.remove(result.size()-1);
+        }
     }
 
 }
