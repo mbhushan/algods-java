8c05f8a4ed4d3a23ee9397d25b34cf06a81d48ae
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-19 10:12:39 -0700

    Setup: Given a set of candidate numbers (candidates) (without duplicates) and a target number (target),

--- rec_18_CombinationSum/src/CombinationSum.java
+++ rec_18_CombinationSum/src/CombinationSum.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+/**
+ Given a set of candidate numbers (candidates) (without duplicates) and a target number (target),
+ find all unique combinations in candidates where the candidate numbers sums to target.
+
+ The same repeated number may be chosen from candidates unlimited number of times.
+
+ Note:
+
+ All numbers (including target) will be positive integers.
+ The solution set must not contain duplicate combinations.
+ Example 1:
+
+ Input: candidates = [2,3,6,7], target = 7,
+ A solution set is:
+ [
+ [7],
+ [2,2,3]
+ ]
+ Example 2:
+
+ Input: candidates = [2,3,5], target = 8,
+ A solution set is:
+ [
+ [2,2,2,2],
+ [2,3,3],
+ [3,5]
+ ]
+ Seen
+ */
 
 public class CombinationSum {
+
+    public static void main(String[] args) {
+        CombinationSum cs = new CombinationSum();
+
+        int [][] candidates = {
+                {2,3,6,7},
+                {2,3,5}
+        };
+
+        int [] targets = {
+                7, 8
+        };
+
+    }
+
+    public void combinationSum()
 }
