2f5e39922223a7c6e130cf8280e2ec581c9bcd3c
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-19 10:14:44 -0700

    Setup: rearrange string chars so that there are no repeated characters near each other

--- pq_NoRepeats/src/NoRepeats.java
+++ pq_NoRepeats/src/NoRepeats.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+/**
+ Rearrange characters in a string such that no two adjacent are same
+ Given a string with repeated characters, task is rearrange characters in a
+ string so that no two adjacent characters are same.
+
+ Note : It may be assumed that the string has only lowercase English alphabets.
+
+ Examples:
+
+ Input: aaabc
+ Output: abaca
+
+ Input: aaabb
+ Output: ababa
+
+ Input: aa
+ Output: Not Possible
+
+ Input: aaaabc
+ Output: Not Possible
+ */
 
 public class NoRepeats {
+
+    public static void main(String[] args) {
+        NoRepeats nr = new NoRepeats();
+
+    }
+
+
 }
