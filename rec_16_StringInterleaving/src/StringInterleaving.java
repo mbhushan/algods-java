28eeba29ce2d480ce5fb0475a03c6887d03ed991
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-18 18:35:04 -0700

    setup: Print all interleavings of given two strings

--- rec_16_StringInterleaving/src/StringInterleaving.java
+++ rec_16_StringInterleaving/src/StringInterleaving.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+/**
+ Print all interleavings of given two strings
+ Given two strings str1 and str2, write a function that prints all interleavings of the given two strings.
+ You may assume that all characters in both strings are different
+
+ Example:
+
+ Input: str1 = "AB",  str2 = "CD"
+ Output:
+ ABCD
+ ACBD
+ ACDB
+ CABD
+ CADB
+ CDAB
+
+ Input: str1 = "AB",  str2 = "C"
+ Output:
+ ABC
+ ACB
+ CAB
+ An interleaved string of given two strings preserves the order of characters in individual strings.
+ For example, in all the interleavings of above first example, ‘A’ comes before ‘B’ and ‘C’ comes before ‘D’.
+ */
 
 public class StringInterleaving {
+
+    public static void main(String[] args) {
+        StringInterleaving si = new StringInterleaving();
+
+    }
+
+    public void findInterleavings(String s1, String s2) {
+
+    }
 }
