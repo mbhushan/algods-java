2b559b869c05397c918e507f38f57cf02765f9fc
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-19 22:31:56 -0700

    Setup: Given a list of list of Strings/Integers. Print cartesian product of lists.

--- rec_19_CartesianProduct/src/CartesianProduct.java
+++ rec_19_CartesianProduct/src/CartesianProduct.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.util.List;
+
+/**
+ * Given a list of list of Strings/Integers. Print cartesian product of lists.
+
+
+ * input -> {"Hello", "World"} , {"Game"}, {"Go","Home"}
+ * output ->
+ * Hello Game Go
+ * Hellow Game Home
+ * World Game Go
+ * World Game Home
+
+ Input : A = {1, 2}, B = {3, 4}
+ Output : A × B = {{1, 3}, {1, 4}, {2, 3}, {2, 4}}
+
+ Input  : A = {1, 2, 3} B = {4, 5, 6}
+ Output : A × B = {{1, 4}, {1, 5}, {1, 6}, {2, 4},
+ {2, 5}, {2, 6}, {3, 4}, {3, 5}, {3, 6}}
+
+ =================
+ INPUT / OUTPUT
+ =================
+
+ */
 
 public class CartesianProduct {
+
+    public static void main(String[] args) {
+        CartesianProduct cp = new CartesianProduct();
+
+    }
+
+    public void cartesianProduct(List<List<String>> inputs) {
+
+    }
 }
