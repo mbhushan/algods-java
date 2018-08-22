a5b6ca9da7d1d0789622617b1d871bf08059b64e
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-21 19:59:10 -0700

    Setup:  Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.

--- rec_24_RemoveInvalidParentheses/src/RemoveInvalidParentheses.java
+++ rec_24_RemoveInvalidParentheses/src/RemoveInvalidParentheses.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+/**
+ Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+
+ Note: The input string may contain letters other than the parentheses ( and ).
+
+ Example 1:
+
+ Input: "()())()"
+ Output: ["()()()", "(())()"]
+ Example 2:
+
+ Input: "(a)())()"
+ Output: ["(a)()()", "(a())()"]
+ Example 3:
+
+ Input: ")("
+ Output: [""]
+
+ =====================
+ INPUT / OUTPUT
+ =====================
+
+ */
 
 public class RemoveInvalidParentheses {
+
+    public static void main(String[] args) {
+        RemoveInvalidParentheses rip = new RemoveInvalidParentheses();
+
+    }
 }
