aa951aec820175b474888c7f5bafd55916a8ba3b
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-22 21:35:48 -0700

    Solution 2 using BFS: Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results

--- rec_24_RemoveInvalidParentheses/src/RemoveInvalidParentheses.java
+++ rec_24_RemoveInvalidParentheses/src/RemoveInvalidParentheses.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
 import java.util.stream.Collectors;
@@ -37,6 +39,20 @@ import java.util.stream.Collectors;
  min edit: 2
  solutions: []
 
+
+ solution using BFS:
+ input string: ()())()
+ bfs result: [(())(), ()()()]
+
+ input string: ()v)
+ bfs result: [(v), ()v]
+
+ input string: (a)())()
+ bfs result: [(a())(), (a)()()]
+
+ input string: )(
+ bfs result: []
+
  */
 
 public class RemoveInvalidParentheses {
@@ -56,6 +72,7 @@ public class RemoveInvalidParentheses {
         for (String e: expressions) {
             rip.parenthesis(e);
         }
+        System.out.println();
 
 //        String [] tests = {"()()", "(()))", "()()()", "(()))"};
 //
@@ -65,6 +82,64 @@ public class RemoveInvalidParentheses {
 //            System.out.println(s + ": " + flag);
 //        }
 
+//        String s = "abcd";
+//        List<Character> list = s.chars().mapToObj(e -> (char)e).collect(Collectors.toList());
+//        System.out.println("list test: " +  list);
+
+        System.out.println();
+        System.out.println("solution using BFS: ");
+        for (String e: expressions) {
+            System.out.println("input string: " + e);
+            rip.rmParenthesisBFS(e);
+            System.out.println();
+        }
+
+    }
+
+    public List<String> rmParenthesisBFS(String input) {
+
+        List<String> result = new ArrayList<String>();
+        Set<String> visited = new HashSet<>();
+
+        Queue<String> queue = new LinkedList<>();
+
+        queue.add(input);
+        visited.add(input);
+
+        boolean flag = false;
+
+        while (!queue.isEmpty()) {
+            String candidate = queue.remove();
+            List<Character> list = candidate.chars().mapToObj(e -> (char)e).collect(Collectors.toList());
+
+
+            if (validParen(list)) {
+                //System.out.println("list: " + list);
+                result.add(candidate);
+                flag = true;
+            }
+
+            if (flag) continue;
+
+            for (int i=0; i<candidate.length(); i++) {
+                if (candidate.charAt(i) != '(' && candidate.charAt(i) != ')') {
+                     continue;
+                }
+
+                String str = candidate.substring(0, i) + candidate.substring(i+1);
+                if (visited.contains(str)) {
+                    continue;
+                }
+                    //System.out.println(str);
+                    visited.add(str);
+                    queue.add(str);
+            }
+        }
+
+        System.out.println("bfs result: " + result);
+       // System.out.println("visited: " + visited);
+
+        return null;
     }
 
     public void parenthesis(String input) {
