c8b244078539d7d6b42920d7d14eebe5b5db7b66
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-23 16:16:14 -0700

    Setup: Write a program to solve a Sudoku puzzle by filling the empty cells.

--- rec_25_SudokuSolver/src/SudokuSolver.java
+++ rec_25_SudokuSolver/src/SudokuSolver.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+/**
+ Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+ A sudoku solution must satisfy all of the following rules:
+
+ Each of the digits 1-9 must occur exactly once in each row.
+ Each of the digits 1-9 must occur exactly once in each column.
+ Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+ Empty cells are indicated by the character '.'.
+
+
+ A sudoku puzzle...
+
+
+ ...and its solution numbers marked in red.
+
+ Note:
+
+ The given board contain only digits 1-9 and the character '.'.
+ You may assume that the given Sudoku puzzle will have a single unique solution.
+ The given board size is always 9x9.
+
+
+ */
 
 public class SudokuSolver {
+
+    public static void main(String[] args) {
+        SudokuSolver ss = new SudokuSolver();
+
+        int [][] board = {
+                {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0}
+        };
+
+    }
+
+    public void sudokuSolver(int [][] M) {
+
+    }
 }
