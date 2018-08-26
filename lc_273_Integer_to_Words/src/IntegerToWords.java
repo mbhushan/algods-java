76bf5a7011bc8dfce4cc5c033de4cdc3f7b621f4
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-25 22:57:30 -0700

    Solution: Integer to English Words

--- lc_273_Integer_to_Words/src/IntegerToWords.java
+++ lc_273_Integer_to_Words/src/IntegerToWords.java
@@ -26,7 +26,26 @@
 
 public class IntegerToWords {
 
+    private static String [] digit1 = {
+            "",
+            "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"
+    };
+
+    private static String [] digit10 = {
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    };
+
+    private static String [] digit2 = {
+            "", "", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninty"
+    };
+
+    private static String [] digit0 = {
+            "", "", "Hundred", "Thousand", "", "", "Million", "", "", "Billion"
+    };
+
     public static void main(String[] args) {
+        IntegerToWords iw = new IntegerToWords();
+
         int [] inputs = {
                 123,
                 12345,
@@ -34,11 +53,117 @@ public class IntegerToWords {
                 1234567891
         };
 
+        int [] digits = {
+                123,
+                345,
+                567,
+                789,
+                890,
+                101,
+                100,
+                500,
+                999,
+                12
+        };
+
+       // System.out.println(iw.getThreeDigitWord(12));
+
+        for (int n: digits) {
+            System.out.println("Input number: " + n);
+            String word = iw.getThreeDigitWord(n);
+            System.out.println("English: " + word);
+            System.out.println();
+        }
+
+      // iw.intToWord(1234567891);
+
+        for (int n: inputs) {
+            System.out.println("Input number: " + n);
+            String word = iw.intToWord(n);
+            System.out.println("English: " + word);
+            System.out.println();
+        }
 
     }
 
     public String intToWord(int n) {
 
-        return null;
+        StringBuffer sb = new StringBuffer();
+
+        String str = String.valueOf(n);
+        int len = str.length();
+
+        int x = -1;
+
+        if (len == 10) {
+            x = n / (int) Math.pow(10, 9);
+            n = n % (int) Math.pow(10, 9);
+
+            sb.append(digit1[x] + " " + digit0[9] + " ");
+
+            len = String.valueOf(n).length();
+        }
+        if (len > 6) {
+            x = n / (int) Math.pow(10, 6);
+            n = n % (int) Math.pow(10, 6);
+
+            String word = getThreeDigitWord(x);
+            sb.append(word + " " + digit0[6] + " ");
+
+            len = String.valueOf(n).length();
+        }
+
+        if (len > 3) {
+            x = n / (int) Math.pow(10, 3);
+            n = n % (int) Math.pow(10, 3);
+
+            String word = getThreeDigitWord(x);
+            sb.append(word + " " + digit0[3] + " ");
+
+            len = String.valueOf(n).length();
+        }
+
+        if (len <= 3) {
+            String word = getThreeDigitWord(n);
+            sb.append(word);
+        }
+
+        //System.out.println("english: " + sb.toString());
+
+        return sb.toString();
+    }
+
+    public String getThreeDigitWord(int n) {
+
+        String str = String.valueOf(n);
+        int len = str.length();
+        int x = -1;
+
+        StringBuffer sb = new StringBuffer();
+
+        if (len == 3) {
+            x = n / 100;
+            n = n % 100;
+            len = String.valueOf(n).length();
+            sb.append(digit1[x] + " " + digit0[2] + " ");
+        }
+        if (len == 2) {
+            x = n / 10;
+            n = n % 10;
+            len = String.valueOf(n).length();
+            if (x >= 2) {
+                sb.append(digit2[x] + " ");
+            } else if (x == 1){
+                sb.append(digit10[n]);
+                len = 0;
+            }
+        }
+        if (len == 1) {
+            sb.append(digit1[n]);
+        }
+
+        //System.out.println("english: " + sb.toString());
+
+        return sb.toString();
     }
 }
