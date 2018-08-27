03fa8b11ddc0a746b76d77669abd327bf33e829b
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-26 21:56:45 -0700

    Solution:  Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?

--- lc_15_ThreeSum/src/ThreeSum.java
+++ lc_15_ThreeSum/src/ThreeSum.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
+
+ 15. 3Sum
+ https://leetcode.com/problems/3sum/description/
+
  Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
  Find all unique triplets in the array which gives the sum of zero.
 
@@ -17,6 +23,12 @@ import java.util.List;
  [-1, 0, 1],
  [-1, -1, 2]
  ]
+
+ ===============
+ INPUT / OUTPUT
+ ===============
+ sorted array: [-4, -1, -1, 0, 1, 2]
+ result: [[-1, -1, 2], [-1, 0, 1]]
  */
 
 public class ThreeSum {
@@ -25,9 +37,51 @@ public class ThreeSum {
         ThreeSum ts = new ThreeSum();
 
         int [] A = {-1, 0, 1, 2, -1, -4};
+
+        ts.threeSum(A);
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+        System.out.println("sorted array: " + Arrays.toString(nums));
+
+        for (int i=0; i<nums.length-2; i++) {
+
+            if (i > 0 && (nums[i] == nums[i-1])) {
+                continue;
+            }
+
+            int target = -1 *  nums[i];
+
+            int j = i+1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                if (nums[j] + nums[k] < target) {
+                    ++j;
+                } else if (nums[j] + nums[k] > target) {
+                    --k;
+                } else { //triplet found.
+                    Integer [] A = new Integer[] {nums[i], nums[j], nums[k]};
+                    List<Integer> list = Arrays.asList(A);
+                    result.add(list);
+                    ++j;
+                    --k;
+                }
+            }
+        }
+
+        System.out.println("result: " + result);
+
+
+        return result;
 
     }
 }
