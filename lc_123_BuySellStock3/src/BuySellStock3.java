7c10e428db2bdfb9861db41e9877446692297d48
Mani Bhushan <manibhushan.cs@gmail.com>
2018-08-31 12:28:12 -0700

    Solution with input/output: 123. Best Time to Buy and Sell Stock III

--- lc_123_BuySellStock3/src/BuySellStock3.java
+++ lc_123_BuySellStock3/src/BuySellStock3.java
@@ -35,6 +35,35 @@ import java.util.Arrays;
  =================
  INPUT / OUTPUT
  =================
+ input: [1, 4, 5, 7, 6, 3, 2, 9]
+ Left: [0, 3, 4, 6, 6, 6, 6, 8]
+ Right: [8, 7, 7, 7, 7, 7, 7, 0]
+ max profit: 13
+
+ input: [3, 3, 5, 0, 0, 3, 1, 4]
+ Left: [0, 0, 2, 2, 2, 3, 3, 4]
+ Right: [4, 4, 4, 4, 4, 3, 3, 0]
+ max profit: 6
+
+ input: [1, 2, 3, 4, 5]
+ Left: [0, 1, 2, 3, 4]
+ Right: [4, 3, 2, 1, 0]
+ max profit: 4
+
+ input: [7, 1, 5, 3, 6, 4]
+ Left: [0, 0, 4, 4, 5, 5]
+ Right: [5, 5, 3, 3, 0, 0]
+ max profit: 7
+
+ input: [7, 6, 4, 3, 1]
+ Left: [0, 0, 0, 0, 0]
+ Right: [0, 0, 0, 0, 0]
+ max profit: 0
+
+ input: [1, 2, 3, 4, 5]
+ Left: [0, 1, 2, 3, 4]
+ Right: [4, 3, 2, 1, 0]
+ max profit: 4
  */
 
 
