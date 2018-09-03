59ad6243d328eae78166b2b5db1cc49936f09fd4
Mani Bhushan <manibhushan.cs@gmail.com>
2018-09-03 10:07:41 -0700

    Setup: Given an array nums, write a function to move all 0's to the end of it while maintaining the  relative order of the non-zero elements.

--- lc_283_MoveZeroes/src/MoveZeroes.java
+++ lc_283_MoveZeroes/src/MoveZeroes.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+/**
 
+ 283. Move Zeroes
+ https://leetcode.com/problems/move-zeroes/description/
+
+ Given an array nums, write a function to move all 0's to the end of it while maintaining the
+ relative order of the non-zero elements.
+ Example:
+ Input: [0,1,0,3,12]
+ Output: [1,3,12,0,0]
+ Note:
+ You must do this in-place without making a copy of the array.
+ Minimize the total number of operations.
+
+
+ */
 public class MoveZeroes {
+
+    public static void main(String[] args) {
+        MoveZeroes mz = new MoveZeroes();
+
+    }
+
+    public void moveZeroes(int[] nums) {
+
+    }
 }
