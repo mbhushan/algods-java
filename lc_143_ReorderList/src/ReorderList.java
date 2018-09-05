b6c9095eff13a5a6e1b6f2f3091d0098a25b9db0
Mani Bhushan <manibhushan.cs@gmail.com>
2018-09-05 15:42:26 -0700

    Solution: Reorder List: Given a singly linked list L: L0→L1→…→Ln-1→Ln, reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…

--- lc_143_ReorderList/src/ReorderList.java
+++ lc_143_ReorderList/src/ReorderList.java
@@ -28,12 +28,70 @@ public class ReorderList {
 
         rl.buildList();
 
-        rl.reverseList();
+        //rl.reverseList();
+
+        rl.reorderList();
 
     }
 
-    public void reorderList(ListNode head) {
+    public void reorderList() {
+        reorderList(this.head);
+    }
 
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        int len = getListLength(head);
+        System.out.println("list to be reordered: ");
+        printList(head);
+
+        int mid = len / 2;
+
+        //System.out.println("mid: " + mid);
+
+        ListNode first = head;
+        ListNode second = head;
+
+        while (mid-- > 0) {
+            second = second.next;
+        }
+        second.next = reverseList(second.next);
+
+        System.out.println("list after reversing second half: ");
+        printList(head);
+
+//        System.out.println("second ptr list");
+//        printList(second);
+
+        while (second.next != null) {
+            ListNode node = second.next;
+            second.next = node.next;
+
+            node.next = first.next;
+            first.next = node;
+            first = node.next;
+//            System.out.println("while: ");
+//            printList(head);
+
+        }
+
+        System.out.println("reordered: ");
+        printList(head);
+
+
+    }
+
+    public int getListLength(ListNode node) {
+        int n = 0;
+
+        while (node != null) {
+            node = node.next;
+            ++n;
+        }
+
+        return n;
     }
 
     public void reverseList() {
@@ -119,6 +177,7 @@ public class ReorderList {
         this.head.next.next = new ListNode(3);
         this.head.next.next.next = new ListNode(4);
         this.head.next.next.next.next = new ListNode(5);
+        this.head.next.next.next.next.next = new ListNode(6);
     }
 
 
