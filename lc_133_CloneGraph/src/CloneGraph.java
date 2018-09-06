3df6dfe8a5a12e15daabf47ade743cd31356b29e
Mani Bhushan <manibhushan.cs@gmail.com>
2018-09-06 07:50:50 -0700

    WIP Solution:  Clone Graph

--- lc_133_CloneGraph/src/CloneGraph.java
+++ lc_133_CloneGraph/src/CloneGraph.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  133. Clone Graph
@@ -31,14 +34,74 @@ import java.util.List;
  */
 public class CloneGraph {
 
+    private UndirectedGraphNode startNode;
+    private UndirectedGraphNode clonedStartNode;
+    private int numVertices;
+
     public static void main(String[] args) {
         CloneGraph cg = new CloneGraph();
+
+        String [] inputs = {
+                "0,1,2#1,2#2,2"
+        };
+
+        cg.buildGraph(inputs[0], 3);
+        cg.printGraph();
 
     }
 
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
 
+
         return null;
+    }
+
+    public void buildGraph(String input, int nVertices) {
+        String [] nodes = input.split("#");
+
+        System.out.println("nodes: " + Arrays.toString(nodes));
+
+        UndirectedGraphNode [] gnodes = new UndirectedGraphNode[nVertices];
+
+        for (int i=0; i<nVertices; i++) {
+            gnodes[i] = new UndirectedGraphNode(i);
+        }
+
+        startNode = gnodes[0];
+        numVertices = nVertices;
+
+        for (int i=0; i<nodes.length; i++) {
+            int [] vertices = Arrays.stream(nodes[i].split(",")).mapToInt(Integer::parseInt).toArray();
+
+            for (int j=1; j<vertices.length; j++) {
+                gnodes[vertices[0]].neighbors.add(gnodes[vertices[j]]);
+            }
+        }
+    }
+
+    private void printGraph() {
+        if (startNode == null) {
+            return;
+        }
+
+        boolean [] visited = new boolean[numVertices];
+        Queue<UndirectedGraphNode> queue = new LinkedList<>();
+
+        queue.add(startNode);
+        visited[startNode.label] = true;
+
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode node = queue.remove();
+            System.out.println(node);
+
+            for (UndirectedGraphNode neighbors: node.neighbors) {
+                if (!visited[neighbors.label]) {
+                    queue.add(neighbors);
+                    visited[neighbors.label] = true;
+                }
+            }
+        }
+
     }
 }
 
@@ -46,4 +109,22 @@ public class CloneGraph {
       int label;
       List<UndirectedGraphNode> neighbors;
       UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+
+      @Override
+      public String toString() {
+          StringBuffer sb = new StringBuffer();
+          sb.append("[" + this.label + " -> ");
+          if (this.neighbors.size() > 0) {
+              List<Integer> nList = new ArrayList<>();
+              for (UndirectedGraphNode n: neighbors) {
+                  nList.add(n.label);
+              }
+              sb.append(nList);
+          } else {
+              sb.append("null");
+          }
+          sb.append("]");
+
+          return sb.toString();
+      }
   };
