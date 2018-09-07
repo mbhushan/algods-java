d9a6254f3df26c8f330966dc8e438cdea2cdb1f2
Mani Bhushan <manibhushan.cs@gmail.com>
2018-09-07 08:20:19 -0700

    Setup: 639. Decode Ways II

--- lc_639_DecodeWays2/src/DecodeWays2.java
+++ lc_639_DecodeWays2/src/DecodeWays2.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+/**
+ 639. Decode Ways II
+ https://leetcode.com/problems/decode-ways-ii/description/
+
+ A message containing letters from A-Z is being encoded to numbers using the following mapping way:
+
+ 'A' -> 1
+ 'B' -> 2
+ ...
+ 'Z' -> 26
+ Beyond that, now the encoded string can also contain the character '*', which can be treated as one
+ of the numbers from 1 to 9.
+
+ Given the encoded message containing digits and the character '*', return the total number of ways to decode it.
+
+ Also, since the answer may be very large, you should return the output mod 109 + 7.
+
+ Example 1:
+ Input: "*"
+ Output: 9
+ Explanation: The encoded message can be decoded to the string: "A", "B", "C", "D", "E", "F", "G", "H", "I".
+ Example 2:
+ Input: "1*"
+ Output: 9 + 9 = 18
+ */
 
 public class DecodeWays2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int numDecodings(String s) {
+
+
+        return 0;
+    }
 }
