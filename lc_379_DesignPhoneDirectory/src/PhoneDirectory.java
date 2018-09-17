f2a94e25acb8e1f2362b566723f6054928f4b955
Mani Bhushan <manibhushan.cs@gmail.com>
2018-09-17 07:52:44 -0700

    Solution:  379. Design Phone Directory

---
+++ lc_379_DesignPhoneDirectory/src/PhoneDirectory.java
@@ -0,0 +1,96 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ 379. Design Phone Directory
+ https://leetcode.com/problems/design-phone-directory/description/
+
+ Design a Phone Directory which supports the following operations:
+
+ get: Provide a number which is not assigned to anyone.
+ check: Check if a number is available or not.
+ release: Recycle or release a number.
+ Example:
+
+ // Init a phone directory containing a total of 3 numbers: 0, 1, and 2.
+ PhoneDirectory directory = new PhoneDirectory(3);
+
+ // It can return any available phone number. Here we assume it returns 0.
+ directory.get();
+
+ // Assume it returns 1.
+ directory.get();
+
+ // The number 2 is available, so return true.
+ directory.check(2);
+
+ // It returns 2, the only number that is left.
+ directory.get();
+
+ // The number 2 is no longer available, so return false.
+ directory.check(2);
+
+ // Release number 2 back to the pool.
+ directory.release(2);
+
+ // Number 2 is available again, return true.
+ directory.check(2);
+
+ */
+
+class PhoneDirectory {
+
+    Set<Integer> set = new HashSet<Integer>();
+    Queue<Integer> queue = new LinkedList<>();
+    int maxNum = 0;
+
+    /** Initialize your data structure here
+     @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
+    public PhoneDirectory(int maxNumbers) {
+        for (int i=0; i<maxNumbers; i++) {
+            // set.add(i);
+            queue.add(i);
+        }
+        maxNum = maxNumbers;
+
+    }
+
+    /** Provide a number which is not assigned to anyone.
+     @return - Return an available number. Return -1 if none is available. */
+    public int get() {
+        if (queue.isEmpty()) {
+            return -1;
+        }
+
+        int res = queue.remove();
+        set.add(res);
+
+        return res;
+
+    }
+
+    /** Check if a number is available or not. */
+    public boolean check(int number) {
+        if (number >= maxNum || number < 0) {
+            return false;
+        }
+        return !set.contains(number);
+    }
+
+    /** Recycle or release a number. */
+    public void release(int number) {
+        if (set.remove(number)) {
+            queue.add(number);
+        }
+    }
+}
+
+/**
+ * Your PhoneDirectory object will be instantiated and called as such:
+ * PhoneDirectory obj = new PhoneDirectory(maxNumbers);
+ * int param_1 = obj.get();
+ * boolean param_2 = obj.check(number);
+ * obj.release(number);
+ */
