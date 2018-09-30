156dcd16c551ea978a458fb8804aa459a64a338c
Mani Bhushan <manibhushan.cs@gmail.com>
2018-09-30 08:28:40 -0700

    Solution: weghted nested list

--- lc_364_NestedListWeightSumII/src/WeightedNestedList.java
+++ lc_364_NestedListWeightSumII/src/WeightedNestedList.java
@@ -79,4 +79,33 @@ class WeightedNestedList {
 
         return weighted;
     }
+
+//    Actually got this from the tagged company phone interview after hinted by the interviewer.
+//    The idea is to deduct number depth - level times.
+//    For example, 1x + 2y + 3z = (3 + 1) * (x + y + z) - (3x + 2y + z);
+//    So we can convert this problem to Nested List Weight Sum I and just record max depth and flat sum at the same time.
+
+    class Solution {
+        public int depthSumInverse(List<NestedInteger> nestedList) {
+            int depthSum = dfs(nestedList, 1);
+            return flatSum * (max + 1) - depthSum;
+        }
+
+        int flatSum = 0;
+        int max = 1;
+        private int dfs(List<NestedInteger> input, int depth) {
+            if (input == null || input.size() == 0) return 0;
+            int sum = 0;
+            for (NestedInteger i : input) {
+                if (i.isInteger()) {
+                    max = Math.max(depth, max);
+                    sum += i.getInteger() * depth;
+                    flatSum += i.getInteger();
+                } else {
+                    sum += dfs(i.getList(), depth + 1);
+                }
+            }
+            return sum;
+        }
+    }
 }
