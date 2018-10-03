5e0e44a6e351d8ffa9cd936c4b398b5924cdcb80
Mani Bhushan <manibhushan.cs@gmail.com>
2018-10-02 18:39:50 -0700

    Solution: pour water

--- lc_755_PourWater/src/PourWater.java
+++ lc_755_PourWater/src/PourWater.java
@@ -1,4 +1,8 @@
 /**
+
+ 755. Pour Water
+ https://leetcode.com/problems/pour-water/description/
+
  We are given an elevation map, heights[i] representing the height of the terrain at that index.
  The width at each index is 1. After V units of water fall at index K, how much water is at each index?
 
