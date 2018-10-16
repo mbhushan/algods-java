cd1eb9ead395af44fa646044542a55ead7bef31c
Mani Bhushan <manibhushan.cs@gmail.com>
2018-10-15 21:27:17 -0700

    Solution:  207. Course Schedule

--- lc_207_CourseSchedule/src/CourseSchedule.java
+++ lc_207_CourseSchedule/src/CourseSchedule.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -59,6 +60,37 @@ public class CourseSchedule {
             if (inDegree[i] != 0)
                 return false;
         }
+        return true;
+    }
+
+    public boolean canFinishDFS(int numCourses, int[][] prerequisites) {
+        ArrayList[] graph = new ArrayList[numCourses];
+        for(int i=0;i<numCourses;i++)
+            graph[i] = new ArrayList();
+
+        boolean[] visited = new boolean[numCourses];
+        for(int i=0; i<prerequisites.length;i++){
+            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+        }
+
+        for(int i=0; i<numCourses; i++){
+            if(!dfs(graph,visited,i))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean dfs(ArrayList[] graph, boolean[] visited, int course){
+        if(visited[course])
+            return false;
+        else
+            visited[course] = true;;
+
+        for(int i=0; i<graph[course].size();i++){
+            if(!dfs(graph,visited,(int)graph[course].get(i)))
+                return false;
+        }
+        visited[course] = false;
         return true;
     }
 }
